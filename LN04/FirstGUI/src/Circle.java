8a99347ea44fd9132c0990a29a0447fe67d626bf
Mustafa Sert <47771042+mustafa-sert@users.noreply.github.com>
2022-07-26 17:16:05 +0300

    Add files via upload

---
+++ LN04/FirstGUI/src/Circle.java
@@ -0,0 +1,48 @@
+public class Circle { 
+	private int x;
+	private int y;
+	private double radius;
+	
+	public Circle() {
+		x = y = 0;
+		radius = 0.0;
+	}	
+	public Circle(int x, int y, double radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+	
+	public Circle(double radius) {
+		this.radius = radius;
+		this.x = this.y = 0;
+	}
+	
+	public int getX() { // reads value of attribute x
+		return this.x; 
+	}	
+	public void setX(int x) { //writes to x
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}	
+	public String toString() { // Override Object's toString method
+		return "Circle(x="+x+", y="+y+", radius="+radius+")";			 
+	}	
+	public double area() {
+		return Math.PI * radius * radius;
+	}	
+	public double circum() {
+		return 2 * Math.PI * radius; 
+	}	
+}
